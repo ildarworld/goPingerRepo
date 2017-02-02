0bfbb15d4d31d195b9c9be33c61a27dde22fefa8
ildarworld <ildarworld@gmail.com>
2017-02-02 14:16:23 +0300

    Init commit

---
+++ pinger/pingGo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "time"
+    "os"
+
+    "github.com/tatsushid/go-fastping"
+)
+
+
+const targetIP = "8.8.8.8"
+
+func main() {
+    p := fastping.NewPinger()
+    ra, err := net.ResolveIPAddr("ip4:icmp", targetIP)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    p.AddIPAddr(ra)
+    p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+        fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+    }
+    p.OnIdle = func() {
+        fmt.Println("finish")
+    }
+    err = p.Run()
+    if err != nil {
+        fmt.Println(err)
+    }
+}
